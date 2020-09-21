460e33e7b611ccc6e30dd43ac7322fe308979218
Sourav Ghosh <sourav.ghosh700@hotmail.com>
2020-09-22 00:26:56 +0530

    bubble sort added

--- Sorting.py
+++ Sorting.py
@@ -1,15 +1,49 @@
+import functools
+import time
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper_timer
+
+
 class Sorting:
-    def bubbleSort(self):
+    @timer
+    def bubbleSort(self, arr, n):
+        for i in range(1, n):
+            for j in range(0, n - i):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        print(f"Sorted list: {arr}")
+
+    @timer
+    def insertionSort(self, arr, n):
         pass
-    def insertionSort(self):
+
+    @timer
+    def selectionSort(self, arr, n):
         pass
-    def selectionSort(self):
+
+    @timer
+    def mergeSort(self, arr, n):
         pass
-    def mergeSort(self):
+
+    @timer
+    def radixSort(self, arr, n):
         pass
-    def radixSort(self):
+
+    @timer
+    def heapSort(self, arr, n):
         pass
-    def heapSort(self):
-        pass
-    def quicksort(self):
+
+    @timer
+    def quicksort(self, arr, n):
         pass
---
+++ test_sorts.py
@@ -0,0 +1,23 @@
+import random
+from Sorting import Sorting
+
+testCases = []
+# small testcase
+testCases.append(random.sample(range(-99, 99), 10))
+# medium testcase
+testCases.append(random.sample(range(-999, 999), 1000))
+# large testcase
+testCases.append(random.sample(range(-9999, 9999), 10000))
+
+for testCase in testCases:
+    n = len(testCase)
+    s = Sorting()
+    print("Initial unsorted list: {}".format(testCase))
+    s.bubbleSort(testCase, n)
+    s.insertionSort(testCase, n)
+    s.selectionSort(testCase, n)
+    s.mergeSort(testCase, n)
+    s.radixSort(testCase, n)
+    s.heapSort(testCase, n)
+    s.quicksort(testCase, n)
+    print("\n\n")
