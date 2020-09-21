9dc2b9f88a385c7363d58072325167a1bbc2ecbc
Sourav Ghosh <sourav.ghosh700@hotmail.com>
2020-09-21 12:33:15 +0530

    Initial commit

---
+++ Sorting.py
@@ -0,0 +1,15 @@
+class Sorting:
+    def bubbleSort(self):
+        pass
+    def insertionSort(self):
+        pass
+    def selectionSort(self):
+        pass
+    def mergeSort(self):
+        pass
+    def radixSort(self):
+        pass
+    def heapSort(self):
+        pass
+    def quicksort(self):
+        pass
